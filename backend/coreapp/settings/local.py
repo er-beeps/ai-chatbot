56bdda12f2fb13789e8bcc21a53a4df35570c26e
er-beeps <bipinkhatiwada10@gmail.com>
2026-07-10 12:45:06 +0545

    updated cors policy

--- backend/coreapp/settings/local.py
+++ backend/coreapp/settings/local.py
@@ -6,3 +6,6 @@ ALLOWED_HOSTS = ["127.0.0.1", "localhost", ".localhost"]
 
 # Override with slower/local email backend
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+CORS_ALLOWED_ORIGINS = config("ALLOWED_ORIGINS", cast=lambda v: [s.strip() for s in v.split(",")])
+CSRF_TRUSTED_ORIGINS = config("ALLOWED_ORIGINS", cast=lambda v: [s.strip() for s in v.split(",")])
--- backend/coreapp/settings/production.py
+++ backend/coreapp/settings/production.py
@@ -9,3 +9,6 @@ SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
 SECURE_BROWSER_XSS_FILTER = True
 SECURE_CONTENT_TYPE_NOSNIFF = True
+
+CORS_ALLOWED_ORIGINS = config("ALLOWED_ORIGINS", cast=lambda v: [s.strip() for s in v.split(",")])
+CSRF_TRUSTED_ORIGINS = config("ALLOWED_ORIGINS", cast=lambda v: [s.strip() for s in v.split(",")])
